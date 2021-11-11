eac38f180fc263e6ccdaccc67ab9298e7e200599
kylecox1 <33430803+kylecox1@users.noreply.github.com>
2021-11-10 21:44:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool playAgain = true;
+            do
+            {
+                SquareExplainer();
+
+                
+                Console.Write("Continue? (y/n): ");
+                playAgain = PlayAgain();
+            } while (playAgain == true);
+            Console.WriteLine("Thanks for playing. Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        // Exercise 45
+        public static void SquareExplainer()
+        {
+            Console.Write("Enter as side length: ");
+            string input = Console.ReadLine();
+            int userLength = int.Parse(input);
+            int userArea = Square.CalculateArea(userLength);
+            int userPerimeter = Square.CalculatePerimeter(userLength);
+            Console.WriteLine($"The square has side length {userLength}. " +
+                $"Its area is {userArea} and its perimeter is {userPerimeter}.");
+        }
+
+        public static bool PlayAgain()
+        {
+            bool isValidYesNo = false;
+            do
+            {
+                string input = Console.ReadLine();
+                if (input.Trim().ToLower() == "n")
+                {
+                    return false;
+                }
+                else if (input.Trim().ToLower() == "y")
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Please just enter a 'y' or an 'n'.");
+                }
+            } while (isValidYesNo == false);
+            return false;
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercises
+{
+    class Square
+    {
+        private int sideLength;
+        public int SideLength
+        {
+            get { return sideLength; }
+            set { sideLength = value; }
+        }
+
+        public Square(int sideLenth)
+        {
+            SideLength = sideLenth;
+        }
+        public static int CalculatePerimeter(int sideLength)
+        {
+            return 4 * sideLength;
+        }
+        public static int CalculateArea(int sideLength)
+        {
+            return sideLength * sideLength;
+        }
+    }
+}
