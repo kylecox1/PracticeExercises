9aaea0d9fbdaa5c2a7e60c04121fd5f463381392
kylecox1 <33430803+kylecox1@users.noreply.github.com>
2021-11-13 10:23:39 -0500

    exercises 31-36

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace PracticeExercises
             bool playAgain = true;
             do
             {
-                TriangleCollector();
+                TwelveDaysOfChristmasSinger();
 
                 Console.Write("Continue? (y/n): ");
                 playAgain = PlayAgain();
@@ -21,6 +21,27 @@ namespace PracticeExercises
             } while (playAgain == true);
             Console.WriteLine("Thanks for playing. Press any key to exit.");
             Console.ReadKey();
+        }
+        public static bool PlayAgain()
+        {
+            bool isValidYesNo = false;
+            do
+            {
+                string input = Console.ReadLine();
+                if (input.Trim().ToLower() == "n")
+                {
+                    return false;
+                }
+                else if (input.Trim().ToLower() == "y")
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Please just enter a 'y' or an 'n'.");
+                }
+            } while (isValidYesNo == false);
+            return false;
         }
 
         // Exercise 45
@@ -133,26 +154,186 @@ namespace PracticeExercises
             Console.WriteLine("");
         }
 
-        public static bool PlayAgain()
+        // Exercise 31
+        public static void DisplayNumberAtIndex()
         {
-            bool isValidYesNo = false;
+            int[] array = { 2, 8, 0, 24, 51 };
+            Console.Write("Enter an index of the array: ");
+            string input = Console.ReadLine();
+            int index = int.Parse(input);
+            if (index < 0 || index > 4)
+            {
+                Console.WriteLine("That is not a valid index.");
+            }
+            else
+            {
+                Console.WriteLine($"The value at index {index} is {array[index]}.");
+            }
+        }
+
+        //Excercise 32
+        public static void DisplayIndexOfNumber()
+        {
+            int[] array = { 2, 8, 0, 24, 51 };
+            Console.Write("Enter an number: ");
+            string input = Console.ReadLine();
+            int selection;
+            if (!int.TryParse(input, out selection))
+            {
+                Console.WriteLine("Please enter an integer number only.");
+            }
+            else if (!array.Contains(selection))
+            {
+                Console.WriteLine("That number was not in the array.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {selection} is located at array position " +
+                    $"{Array.IndexOf(array, selection)}.");
+            }
+        }
+
+        // Exercise 33
+        public static void CustomizeArray()
+        {
+
+            int[] array = { 2, 8, 0, 24, 51 };
+            bool playAgain = true;
             do
             {
+                bool validIndex = false;
+                int selection;
+                do
+                {
+                    Console.Write("Enter an index of the array: ");
+                    string input = Console.ReadLine();
+                    if (!int.TryParse(input, out selection))
+                    {
+                        Console.WriteLine("Please enter an integer number only.");
+                    }
+                    else if (selection < 0 || selection > array.Length - 1)
+                    {
+                        Console.WriteLine("That was out of range");
+                    }
+                    else
+                    {
+                        validIndex = true;
+                    }
+                } while (!validIndex);
+
+                Console.Write($"The value at index {selection} is {array[selection]}. " +
+                    $"Would you like to change it? (y/n)? ");
+
+                bool changeIt = true;
+                changeIt = PlayAgain();
+                if (changeIt)
+                {
+
+                    int replacementNumber;
+                    bool validReplacement = false;
+                    do
+                    {
+                        Console.Write($"Enter the new value at index {selection}: ");
+                        string input = Console.ReadLine();
+                        if (!int.TryParse(input, out replacementNumber))
+                        {
+                            Console.WriteLine("Please enter an integer number only.");
+                        }
+                        else
+                        {
+                            validReplacement = true;
+                        }
+                    } while (!validReplacement);
+
+                    array[selection] = replacementNumber;
+                    Console.WriteLine($"The value at index {selection} is now {array[selection]}.");
+                }
+
+                Console.Write("Would you like to continue (y/n)? ");
+                playAgain = PlayAgain();
+            } while (playAgain);
+        }
+
+        // Exercise 34
+        public static void ModifyArray()
+        {
+            bool playAgain = true;
+            int[] array = { 16, 32, 64, 128, 256 };
+            do
+            {
+                Console.Write("Enter a command (half/double): ");
                 string input = Console.ReadLine();
-                if (input.Trim().ToLower() == "n")
+                if (input.ToLower().Trim() == "half")
                 {
-                    return false;
+                    int i = 0;
+                    foreach (int number in array)
+                    {
+                        array[i] = number / 2;
+                        i++;
+                    }
                 }
-                else if (input.Trim().ToLower() == "y")
+                else if (input.ToLower().Trim() == "double")
                 {
-                    return true;
+                    int i = 0;
+                    foreach (int number in array)
+                    {
+                        array[i] = number * 2;
+                        i++;
+                    }
                 }
-                else
+                Console.WriteLine($"The array now contatins: " +
+                    $"{array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}");
+
+                Console.Write("Would you like to continue (y/n)? ");
+                playAgain = PlayAgain();
+            } while (playAgain);
+        }
+
+        // Exercise 35
+        public static void StringIndexer()
+        {
+            string[] animals = { "cow", "elephant", "jaguar", "horse", "crow" };
+            Console.Write("Enter two indices: ");
+            string input = Console.ReadLine();
+            int[] userSelections = input.Split(' ').Select(int.Parse).ToArray();
+            if (userSelections.Length > 2)
+            {
+                Console.WriteLine("That's more that two.");
+            }
+            int userValue = userSelections[0];
+            int userLetter = userSelections[1];
+            try
+            {
+                Console.WriteLine($"The value at index {userValue} is { animals[userValue] }. " +
+                    $"The letter at index {userLetter} is { animals[userValue].ToArray()[userLetter] }.");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Those are not valid indices.");
+            }
+        }
+
+        // Exercise 36
+        public static void TwelveDaysOfChristmasSinger()
+        {
+            int[] numbers = { 12, 11, 10, 9, 8 };
+            string[] actors = { "Drummers Drumming", "Pipers Piping", "Lords a-Leaping",
+                "Ladies Dancing", "Maids a-Milking" };
+            Console.Write("Enter a command (sing/quit): "); ;
+            string input = Console.ReadLine();
+            if (input.Trim().ToLower() == "quit")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else if (input.Trim().ToLower() == "sing")
+            {
+                int i = 0;
+                foreach (int number in numbers)
                 {
-                    Console.WriteLine("Please just enter a 'y' or an 'n'.");
+                    Console.WriteLine($"{number} {actors[i]}");
+                    i++;
                 }
-            } while (isValidYesNo == false);
-            return false;
+            }
         }
     }
 }
